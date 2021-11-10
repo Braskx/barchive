ffb45250a75fa8e2287dc16a668021e2fae264dc
Braskx <bwf@xplornet.ca>
2021-11-10 15:11:14 -0500

    added case statements for interactive prompts

--- barchive.sh
+++ barchive.sh
@@ -3,22 +3,20 @@
 # BARCHIVE - The simple archiver
 # Coded By: Bryson Forget :^)
 
-#TODO 
-# barchive [-gz/-bz] [name] [local] [remote]
-# create simple man page
-
 clear
-echo "Welcome to the Barchive Backup Script (^_^)" && sleep 1
+echo "Welcome to Bryson's Backup Script (^_^)" && sleep 1
 echo "Keep in mind that this program only uses absolute paths" && sleep 1
 
-#functions (look at the right to see the variables they create)
+#functions to create paths and names
 func_mypath(){
 read -p "What directory are you looking to backup: " mypath 
 }
 func_myname(){
 read -p "What is the file name: " myname
 }
+#function that checks for $cprog variable and compresses accordingly
 func_compress(){
+	echo "FUCK"
 if [ $cprog == "GZ" ]
 	then
 	tar -zcvf $myname $mypath
@@ -33,6 +31,7 @@ mv $myname $filelocation
 func_filelocation(){
 read -p "Where will the backup be stored: " filelocation
 }
+#Checks for cprog variable to compress with func_compress
 func_compressprog(){
 cprog_long="Default"
 read -p "What compression program will be used (GZ or BZ): " cprog
@@ -45,11 +44,12 @@ if [ $cprog == "BZ" ]
 	cprog_long="BZIP2"
 fi
 }
-#init function (it starts the program after clear, and echo commands)
+#init function starts the program (check last line)
 func_init(){
 unset yn
 unset yn2
 func_mypath
+#if the user changes to a directory that doesn't exist, return true
 while !(cd $mypath 2>/dev/null) && echo "Invalid Syntax"
 do
 	func_mypath
@@ -60,6 +60,7 @@ do
 	func_filelocation
 done
 func_compressprog
+# if cprog doesn't return GZ or BZ, return true
 while !([ $cprog == GZ ] 2>&1>/dev/null || [ $cprog == BZ ] 2>&1>/dev/null) && echo "Invalid Syntax"
 do
 	func_compressprog
@@ -67,10 +68,26 @@ done
 func_myname
 echo -e "ORIGINAL: $mypath     NAME: $myname     REMOTE: $filelocation     COMPRESS:$cprog_long"
 read -p ":: Is this correct [Y/N] " yn
+#cases allow multiple conditions to obtain a Yes answer
+case $yn in
+	Y | y | Yes | yes)
+	yn="Y"
+	;;
+	*)
+	yn="N"
+esac
 [ $yn = "Y" ] && echo "Proceeding..." && sleep 1 || echo -e "Aborting...\n" 
-[ ! $yn = "Y" ] && read -p ":: Would you like to restart [Y/N]" yn2
+#second case statement needs to be added after checking that yn doesn't equal Y
+[ ! $yn = "Y" ] && read -p ":: Would you like to restart [Y/N] " yn2 && case $yn2 in
+	Y | y | Yes | yes)
+	yn2="Y"
+	;;
+	*)
+	yn2="N"
+esac
 [ $yn2 = "Y" ] && func_init
 [ ! $yn2 = "Y" ] && exit
 func_compress
 }
+#function that starts the program
 func_init
